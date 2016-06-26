a2dc67551eddf58b1a591d68491ea23cb5c51070
Gustavo Villela Taufic <gustavovillela@MacBook-Pro-de-Gustavo-Villela.local>
2016-06-25 22:38:56 -0300

    Versao Beta quase concluída, falta acabamentos no design, nocao de proximidade camera-prefab e fim de jogo

--- Assets/CameraLogic.cs
+++ Assets/CameraLogic.cs
@@ -5,35 +5,55 @@ using System.Collections.Generic;
 public class CameraLogic : MonoBehaviour
 {
 	
-	public Texture buttonTexture;
-	public Rect buttonLeftPos, buttonRightPos; 
+	public Texture buttonRightTex, buttonLeftTex, barTex, pinTex;
+	public Rect buttonLeftPos, buttonRightPos,barPos, pinPos, scoreLabelPos;
+	private int score = 0;
+	private bool left = false, gamePaused = true;
+	private Rect lastPinPos;
 
-	// Use this for initialization
+
 	void Start ()
 	{ 
 		buttonLeftPos = new Rect (0, Screen.height / 2 - 25, 50, 50);
 		buttonRightPos = new Rect (Screen.width - 50, Screen.height / 2 - 25, 50, 50);
+		barPos = new Rect (0, Screen.height - 10, Screen.width, 10);
+		pinPos = new Rect (Screen.width/2 - 10, Screen.height - 30, 20, 20);
+		lastPinPos = new Rect (Screen.width/2 - 10, Screen.height - 30, 20, 20);
+		scoreLabelPos = new Rect (0, 20, 100, 20);
 	}
 	void OnGUI()
 	{
 		GUI.backgroundColor = Color.clear;
-
-		if (GUI.Button (buttonLeftPos, buttonTexture)) { //left button
-			Debug.LogFormat ("left");
+	
+		if (GUI.Button (buttonLeftPos, buttonLeftTex)) { //left button
+			gamePaused = false;
+			left = true;
 		}
 
-		if (GUI.Button (buttonRightPos, buttonTexture)) { //right button
-			Debug.LogFormat ("right");
+		if (GUI.Button (buttonRightPos, buttonRightTex)) { //right button
+			gamePaused = false;
+			left = false;
 		}
 	
-		GUI.DrawTexture (buttonLeftPos, buttonTexture);
-		GUI.DrawTexture (buttonRightPos, buttonTexture);
+		GUI.TextField (scoreLabelPos, score.ToString ());
+		GUI.DrawTexture (buttonLeftPos, buttonLeftTex);
+		GUI.DrawTexture (buttonRightPos, buttonRightTex);
+		GUI.DrawTexture (barPos, barTex);
+		GUI.DrawTexture (pinPos, pinTex);
 	}
 
-	// Update is called once per frame
 	void Update ()
 	{
+		if (left && !gamePaused) // left button pressed
+			pinPos.x = pinPos.x - 2;
 
+		if (!left && !gamePaused) // right button pressed
+			pinPos.x = pinPos.x + 2;
+
+		// score logic
+		if ((lastPinPos.center.x >= (Screen.width/2) && pinPos.center.x < (Screen.width/2)) ||(lastPinPos.center.x < (Screen.width/2) && pinPos.center.x >= (Screen.width/2)))
+			score++;
+		lastPinPos = pinPos;
 	}
 }
 
